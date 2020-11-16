173f5ba1faa85fa50cf1bf622fc07f49a8c21843
Avigail Yehieli <abiy103@gmail.com>
2020-11-16 21:12:27 +0200

    filter checkbox fix

--- src/cmps/Filter.jsx
+++ src/cmps/Filter.jsx
@@ -5,116 +5,61 @@ class Filter extends Component {
     state = {
         diet: null,
         type: null,
-        intolerances: null
+        intolerances: []
     }
 
-    handleChange = (ev) => {
-        const category = ev.target.name
-        const value =  ev.target.value
-        this.setState( ({ [category]:value}), () => {
-            this.props.onSetFilter(this.state)
-        })
+    componentDidMount() {
+        this.checkState()
     }
 
-
-    // componentDidMount() {
-    //     this.readURL()
-    // }
-
-    readURL = () => {
-        var url = this.props.location.search.substring(3)
-        var strMap = {}
-        //organize url into object
-        var parts = url.split('&')
-        parts.forEach(part => {
-            part = part.split('=')
-            var category = part[0]
-            var value = part[1]
-            if (category === 'intolerances') {
-                if (strMap.intolerances) {
-                    strMap.intolerances.push(value)
-                }else{
-                        value=value.split(',')
-                        strMap.intolerances = value
-                } 
-            }else{
-                strMap[category] = value
-            }
-        })
-
-        //set current state
-        this.setState({...strMap})
-    }
-
-    // handleChange = (ev) => {
-        // var { category, value } = ev.target;
-        // this.changeState(category, value)
-        // var newURL = this.getNewURL(category, value)
-
-        // history.push({
-        //     search: `${newURL}`
-        // })
-    // }
-
-    changeState = (category, value) => {
-        if (category === 'intolerances') {
-            var intolerances = this.state.intolerances
-            var idx = intolerances.findIndex(intolerance => intolerance === value)
-            if (idx === -1) {
-                intolerances.push(value)
-            } else {
-                intolerances.splice(idx, 1)
-            }
-            this.setState({ intolerances })
-        } else {
-            if (this.state[category] === value) {
-                value = ''
-            }
-            this.setState({ [category]: value })
-        }
-    }
-
-    getNewURL = (newCategory, newValue) => {
-        var str = this.props.location.search.substring(3)
-        console.log('curr url', str);
-        var strMap = {}
-        var newStr = ''
-        //dismember
-        var parts = str.split('&')
-        parts.forEach(part => {
-            part = part.split('=')
-            var category = part[0]
-            var value = part[1]
-            if (strMap[category]) {
-
-            }
-            strMap[category] = value
-            if (!strMap[newCategory]) {
-                strMap[newCategory] = [newValue]
-            }
-            if (strMap[newCategory]) {
-                var idx = strMap[newCategory].findIndex(category => category === newValue)
-                if (idx === -1) {//not found - add
-                    strMap[newCategory].push(newValue)
-                } else {//found- remove
-                    strMap[newCategory] = strMap[newCategory].splice(idx, 1)
+    componentDidUpdate() {
+        // console.log('filter cmp updated', this.props.currFilter);
+        var { currFilter } = this.props
+        var { diet, type, intolerances } = this.state
+        var flag = false
+        if (intolerances[0] && currFilter.intolerances[0]) {
+            intolerances.forEach(intolerance => {
+                //if intolarence array is not the same flag is true
+                if (!currFilter.intolerances.includes(intolerance)) {
+                    flag = true
                 }
-            }
-        });
-        //join&return
-        for (const cat in strMap) {
-            newStr = newStr + '&' + cat + '=' + strMap[cat].join(',')
+            })
         }
-        console.log('after change state', this.state);
-        console.log('after change url', str);
-        return newStr.substring(1)
+        if (intolerances[0] && !currFilter.intolerances[0] || !intolerances[0] && currFilter.intolerances[0]) {
+            flag = true
+        }
+        if (flag || currFilter.diet !== diet || currFilter.type !== type) {
+            this.checkState()
+        }
+    }
+
+    checkState = () => {
+        var { currFilter } = this.props
+        this.setState({ ...currFilter }, () => {
+            console.log('filter cmp mounted ', this.state);
+        })
+    }
+
+    handleSelect = (ev) => {
+        const category = ev.target.name
+        const value = ev.target.value
+        console.log('handle select ', category, value);
+        if (category === 'intolerances') {
+            this.setState(prevState => ({ [category]: [...prevState[category], value] }),
+                () => { this.props.handleChange(category, value) });
+        } else {
+            this.setState(({ [category]: value }), () => {
+                this.props.handleChange(category, value)
+            })
+        }
     }
 
     render() {
-        const diets = ['Vegan', 'Vegetarian', 'Paleo']
-        const types = ['Main Course', 'Side Dish', 'Dessert', 'Appetizer']
-        const intolerances = ['Dairy', 'Egg', 'Peanut', 'Gluten']
-        console.log('the state ',this.state)
+        const diets = ['vegan', 'vegetarian', 'paleo']
+        const types = ['main course', 'side dish', 'dessert', 'appetizer']
+        const intolerances = ['dairy', 'egg', 'peanut', 'gluten']
+        const { currFilter } = this.props
+        console.log('filter cmp state ', this.state);
         return (
             <section className={"filter "}>
                 {this.props.mobile && <button className="close-btn" onClick={() => { this.props.showFilter(false) }}>&times;</button>}
@@ -124,10 +69,11 @@ class Filter extends Component {
                             diets.map((diet, idx) => {
                                 return (<label key={diet + idx}>
                                     <input type="checkbox"
-                                        checked={(this.state.diet===diet)}
-                                        disabled={this.state.diet&&(this.state.diet!==diet)}
-                                        onChange={this.handleChange}
-                                        id={diet + idx} category="diet"
+                                        checked={(currFilter.diet === diet)}
+                                        disabled={currFilter.diet && (currFilter.diet !== diet)}
+                                        onChange={(ev) => this.handleSelect(ev)}
+                                        id={diet + idx}
+                                        name="diet"
                                         value={diet} />
                                     <span>{diet}</span>
                                 </label>)
@@ -139,10 +85,10 @@ class Filter extends Component {
                             types.map((type, idx) => {
                                 return (<label key={type + idx}>
                                     <input type="checkbox"
-                                        // checked={}
-                                        // disabled={!type.isChecked}
-                                        onChange={this.handleChange}
-                                        category="type"
+                                        checked={(this.state.type === type)}
+                                        disabled={this.state.type && (this.state.type !== type)}
+                                        onChange={(ev) => this.handleSelect(ev)}
+                                        name="type"
                                         value={type} />
                                     <span>{type}</span>
                                 </label>)
@@ -154,10 +100,9 @@ class Filter extends Component {
                             intolerances.map((intolerance, idx) => {
                                 return (<label key={intolerance + idx}>
                                     <input type="checkbox"
-                                        // disabled={false}
-                                        // checked={intolerance}
-                                        onChange={this.handleChange}
-                                        category="intolerances"
+                                        checked={(currFilter.intolerances[0] && currFilter.intolerances.includes(intolerance))}
+                                        onChange={(ev) => this.handleSelect(ev)}
+                                        name="intolerances"
                                         value={intolerance} />
                                     <span>{intolerance} free</span>
                                 </label>)
--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -4,7 +4,12 @@ import { Link } from 'react-router-dom'
 import Filter from '../cmps/Filter';
 export default class SearchPage extends Component {
     state = {
-        currFilter: {},
+        currFilter: {
+            diet: '',
+            type: '',
+            intolerances: [],
+            query: ''
+        },
         currFilterStr: '',
         currRecipes: [],
         isShown: null,
@@ -16,7 +21,7 @@ export default class SearchPage extends Component {
         this.checkIsMobile()
         window.addEventListener("resize", this.checkIsMobile);
     }
-    
+
     componentWillUnmount() {
         window.removeEventListener("resize", this.checkIsMobile)
     }
@@ -27,17 +32,77 @@ export default class SearchPage extends Component {
 
     checkFilter = () => {
         if (this.state.currFilterStr !== this.props.location.search) {
+            //update state-  str, currFilter
             this.setState({ currFilterStr: this.props.location.search }, () => { this.loadRecipes() })
+            var str = this.props.location.search.substring(3)
+            var parts = str.split('&')
+            parts.forEach(part => {
+                console.log('check filter got ', part);
+                part = part.split('=')
+                var category = part[0]
+                var value = part[1]
+                if (category === 'diet' || category === 'type' || category === 'query') {
+                    console.log(category, value);
+                    //set new type/diet
+                    this.setState(prevState => ({ currFilter: { ...prevState.currFilter, [category]: value } }),
+                        () => { console.log('state filter ', this.state.currFilter) })
+                } else {
+                    if (value.includes(',')) {
+                        //some vals
+                        this.setState(prevState => ({ currFilter: { ...prevState.currFilter, [category]: value.split(',') } }))
+                    } else {
+                        //one val 
+                        this.setState({ currFilter: { ...this.state.currFilter, [category]: [value] } })
+                    }
+                }
+            });
         }
     }
 
-    onSetFilter=(newFilter)=>{
-        this.setState({currFilter:newFilter})
-        var str = ''
-        for(var category in newFilter){
-            str= str+'&'+category +'='+newFilter[category]
+    handleChange = (category, value) => {
+        console.log('handle change got ', category, value);
+        //if null now
+        if (category === 'intolerances') {
+            value = [...this.state.currFilter.intolerances, value]
+            this.setState(prevState => ({ currFilter: { ...prevState.currFilter, [category]: value } }),
+                () => { this.changeUrl() })
+        } else {
+            this.setState({ currFilter: { ...this.state.currFilter, [category]: value } }, () => { this.changeUrl() })
         }
-        this.props.history.push(str)
+    }
+
+    changeUrl = () => {
+        var str = ''
+        var { currFilter } = this.state
+        console.log('got to change url', currFilter);
+        for (var category in currFilter) {
+            if (category === 'intolerances') {
+                if (currFilter[category][0]) {
+                    //if str is not empty
+                    if (str) {
+                        str = str + '&'
+                    }
+                    str = str + 'intolerances='
+                    currFilter[category].forEach((value, idx) => {
+                        //if it's not the last allergy add a ','
+                        if (idx + 1 < currFilter[category].length) {
+                            value = value + ','
+                        }
+                        str = str + value
+                    })
+                }
+                //for diet & type
+            } else if (currFilter[category]) {
+                //if str is not empty
+                if (str) {
+                    str = str + '&'
+                }
+                str = str + category + '=' + currFilter[category]
+            }
+        }
+        this.props.history.push({
+            search: `?q=${str}`
+        })
     }
 
     checkIsMobile = () => {
@@ -51,14 +116,14 @@ export default class SearchPage extends Component {
     }
 
     showFilter = (isShown) => {
-        this.setState({ isShown:isShown })
+        this.setState({ isShown: isShown })
     }
 
     render() {
         const { isMobile, isShown } = this.state
-        var filter=''
-        if (!isMobile||(isMobile&&isShown)){
-            filter = <Filter isMobile={isMobile} showFilter={this.showFilter} location={this.props.location} currFilter={this.state.currFilter} onSetFilter={this.onSetFilter}/>
+        var filter = ''
+        if (!isMobile || (isMobile && isShown)) {
+            filter = <Filter isMobile={isMobile} showFilter={this.showFilter} location={this.props.location} currFilter={this.state.currFilter} handleChange={this.handleChange} />
         }
         return (!this.state.currRecipes) ? <p>Loading</p> : (
             <section className="grid-container">
